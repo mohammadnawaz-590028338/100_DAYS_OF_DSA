b6f233b6e277afcac001acf90c9d8838b92e62dd
mohammadnawaz-590028338 <MOHAMMAD.28338@stu.upes.ac.in>
2026-03-22 17:37:34 +0530

    Create day44.c

---
+++ day44.c
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to build tree from level order
+Node* buildTree(int arr[], int n) {
+    if (n == 0) return NULL;
+
+    Node* root = new Node(arr[0]);
+    queue<Node*> q;
+    q.push(root);
+
+    int i = 1;
+    while (i < n) {
+        Node* curr = q.front();
+        q.pop();
+
+        // Left child
+        if (arr[i] != -1) {
+            curr->left = new Node(arr[i]);
+            q.push(curr->left);
+        }
+        i++;
+
+        // Right child
+        if (i < n && arr[i] != -1) {
+            curr->right = new Node(arr[i]);
+            q.push(curr->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// Traversals
+void inorder(Node* root) {
+    if (!root) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void preorder(Node* root) {
+    if (!root) return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(Node* root) {
+    if (!root) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    Node* root = buildTree(arr, n);
+
+    inorder(root);
+    cout << endl;
+
+    preorder(root);
+    cout << endl;
+
+    postorder(root);
+    cout << endl;
+
+    return 0;
+}
